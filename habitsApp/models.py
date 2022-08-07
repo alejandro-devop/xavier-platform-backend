ae6eebe49edbad128cf1c5750cb3950fb73ebe93
Jorge Alejandro Quiroz Serna <alejandro.devop@gmail.com>
2022-08-07 13:18:24 -0500

    Update models.py
    Added model for habit categories

--- habitsApp/models.py
+++ habitsApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class HabitCategory(models.Model):
+    """Model for the habit categories"""
+    # Name for the habit category
+    name = models.CharField(max_length=200)
+    # Brief description for the habit category
+    description = models.TextField(max_length=800)
+    # A fontawesome icon name
+    icon = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
