5676a103cf85bc614eea9bb23bfd3c466a510b0f
Jorge Alejandro Quiroz Serna <alejandro.devop@gmail.com>
2022-08-07 13:32:31 -0500

    Initialized settingsApp

--- xavierApi/settings.py
+++ xavierApi/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'activitiesApp',
-    'habitsApp'
+    'habitsApp',
+    'settingsApp'
 ]
 
 MIDDLEWARE = [
