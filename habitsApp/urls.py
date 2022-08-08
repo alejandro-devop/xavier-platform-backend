7039602ded73e0649558a7a3c0c433238475fb28
Jorge Alejandro Quiroz Serna <alejandro.devop@gmail.com>
2022-08-08 16:36:12 -0500

    Added detail, update, remove for habits

--- habitsApp/urls.py
+++ habitsApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HabitCategoryApiList, HabitCategoryApiDetail, HabitApiList
+from .views import HabitCategoryApiList, HabitCategoryApiDetail, HabitApiList, HabitApiDetail
 
 urlpatterns = [
     path('categories/', HabitCategoryApiList.as_view()),
     path('categories/<int:item_id>', HabitCategoryApiDetail.as_view()),
-    path('', HabitApiList.as_view())
+    path('', HabitApiList.as_view()),
+    path('<int:item_id>', HabitApiDetail.as_view())
 ]
--- habitsApp/views.py
+++ habitsApp/views.py
@@ -137,3 +137,68 @@ class HabitApiList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class HabitApiDetail(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, item_id, *args, **kwargs):
+        instance = Habit.get_object(request.user.id, item_id)
+        if not instance:
+            return Response({
+                'error': True,
+                'message': 'The object does not exists'
+            })
+        serializer = HabitSerializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+    def put(self, request, item_id, *args, **kwargs):
+        instance = Habit.get_object(request.user.id, item_id)
+        if not instance:
+            return Response({
+                'error': True,
+                'message': 'The object does not exists'
+            })
+
+        data = {
+            'user': request.user.id,
+            'activity': request.data.get('activity'),
+            'measure': request.data.get('measure'),
+            'name': request.data.get('name'),
+            'description': request.data.get('description'),
+            'should_avoid': request.data.get('should_avoid'),
+            'should_keep': request.data.get('should_keep'),
+            'is_counter': request.data.get('is_counter'),
+            'is_timer': request.data.get('is_timer'),
+            'days': request.data.get('days'),
+            'start_date': request.data.get('start_date'),
+            'end_date': request.data.get('end_date')
+        }
+
+        serializer = HabitSerializer(instance=instance, data=data)
+
+        if Habit.already_exists(data['name'], data['user'], item_id):
+            return Response({
+                'error': True,
+                'message': 'The habit is already registered'
+            })
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, item_id, *args, **kwargs):
+        instance = Habit.get_object(request.user.id, item_id)
+        if not instance:
+            return Response({
+                'error': True,
+                'message': 'The object does not exists'
+            })
+        instance.delete()
+        return Response({
+            'removed': True,
+        }, status=status.HTTP_200_OK)
