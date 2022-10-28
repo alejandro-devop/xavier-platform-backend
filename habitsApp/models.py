d960b6ee05ef94c84cd47f8a724489f864ec4988
Jorge Alejandro Quiroz Serna <alejandro.devop@gmail.com>
2022-10-28 17:07:51 -0500

    Added habits follow up

--- habitsApp/models.py
+++ habitsApp/models.py
@@ -4,6 +4,36 @@ from settingsApp.models import HabitMeasures
 from django.contrib.auth.models import User
 
 
+class HabitType(models.Model):
+    # Owner for the habit category
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # Name for the habit category
+    name = models.CharField(max_length=200)
+
+    is_number_goal = models.BooleanField(blank=True, null=True, default=False)
+    is_quantity_goal = models.BooleanField(blank=True, null=True, default=False)
+    is_duration_goal = models.BooleanField(blank=True, null=True, default=False)
+    is_distance_goal = models.BooleanField(blank=True, null=True, default=False)
+
+    @staticmethod
+    def it_already_registered(name, user_id, item_id=None):
+        if item_id is not None:
+            items = HabitType\
+                .objects\
+                .filter(name__iexact=name, user=user_id)\
+                .exclude(id=item_id)
+        else:
+            items = HabitType.objects.filter(name__iexact=name, user=user_id)
+        return len(items) > 0
+
+    @staticmethod
+    def get_object(user_id, item_id):
+        try:
+            return HabitCategory.objects.get(id=item_id, user = user_id)
+        except HabitCategory.DoesNotExist:
+            return None
+
+
 class HabitCategory(models.Model):
     """Model for the habit categories"""
     # Owner for the habit category
@@ -47,6 +77,8 @@ class Habit(models.Model):
     measure = models.ForeignKey(HabitMeasures, on_delete=models.SET_NULL, blank=True, null=True)
     # Activity category
     category = models.ForeignKey(HabitCategory, on_delete=models.SET_NULL, blank=True, null=True)
+
+    type = models.ForeignKey(HabitType, on_delete=models.SET_NULL, blank=True, null=True)
     # Name for the habit
     name = models.CharField(max_length=200)
     # Brief description for the habit
@@ -59,6 +91,8 @@ class Habit(models.Model):
     is_counter = models.BooleanField(default=False)
     # If the habit is a timer
     is_timer = models.BooleanField(default=False)
+    is_incremental = models.BooleanField(default=False, null=True, blank=True)
+    is_decremental = models.BooleanField(default=False, null=True, blank=True)
     # The estimated duration for the habit (number of steps to success)
     days = models.IntegerField(default=90)
     # The current habit streak, it will be reset when no accomplished
@@ -67,8 +101,9 @@ class Habit(models.Model):
     max_streak = models.IntegerField(default=0)
     # Started date for the habit
     start_date = models.DateField()
-    # Estimated finalization for the habit, is the sum of the started date plus dayss
+    # Estimated finalization for the habit, is the sum of the started date plus days
     end_date = models.DateField()
+    daily_goal = models.IntegerField(default=0, null=True, blank=True)
 
     @staticmethod
     def already_exists(name, user_id, item_id=None):
@@ -90,3 +125,15 @@ class Habit(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class HabitFollowUp(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    date = models.DateField()
+    started_date = models.DateTimeField(blank=True, null=True)
+    description = models.CharField(max_length=500)
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE, blank=True, null=True)
+    time_spent = models.IntegerField(default=0)
+    daily_goal = models.IntegerField(blank=True, null=True, default=0)
+    is_accomplished = models.BooleanField(blank=True, null=True, default=False)
+    is_failed = models.BooleanField(blank=True, null=True, default=False)
